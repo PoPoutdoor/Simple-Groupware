0a2440f4f2c1daf6a0a7aeb53d31239a38bb992c
Thomas Bley <thbley+github@gmail.com>
2012-02-12 06:48:41 +0100

    added copyright notes

--- build/release.php
+++ build/release.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @package Simple Groupware
+ * @link http://www.simple-groupware.de
+ * @author Thomas Bley
+ * @copyright Copyright (C) 2002-2012 by Thomas Bley
+ * @license GPLv2
+ */
 
 // TODO validate german translations
 // TODO test install mysql, postgres, sqlite
