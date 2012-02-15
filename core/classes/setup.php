09c11474de55e9919711c488e67d03a0aedb6624
Thomas Bley <thbley+github@gmail.com>
2012-02-15 15:30:49 +0100

    cleanup

--- core/classes/setup.php
+++ core/classes/setup.php
@@ -1,5 +1,11 @@
 <?php
-// GPL
+/**
+ * @package Simple Groupware
+ * @link http://www.simple-groupware.de
+ * @author Thomas Bley
+ * @copyright Copyright (C) 2002-2012 by Thomas Bley
+ * @license GPLv2
+ */
 
 class setup {
 
