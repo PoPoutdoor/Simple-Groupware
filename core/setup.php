9785fad52c7d6e5c1c6f9ac1076aa2bee57234a3
Thomas Bley <thbley+github@gmail.com>
2012-03-04 22:30:50 +0100

    cleanup

--- core/setup.php
+++ core/setup.php
@@ -10,6 +10,7 @@
 if (!defined("MAIN_SCRIPT")) exit;
 
 @ini_set("display_errors","1");
+@ini_set("output_buffering","0");
 @set_time_limit(1800);
 
 header("Cache-Control: private, max-age=1, must-revalidate");
@@ -26,7 +27,7 @@ define("USE_SYSLOG_FUNCTION",0);
 define("CHMOD_DIR",777);
 define("CHMOD_FILE",666);
 define("DB_SLOW",0.5);
-define("LANG","en");
+define("LANG",!empty($_REQUEST["lang"])?$_REQUEST["lang"]:"en");
 
 define("NOW",time());
 define("DEBUG",false);
@@ -41,12 +42,12 @@ define("VIRUS_SCANNER","");
 if (strpos(PHP_OS,"WIN")!==false) $sep = ";"; else $sep = ":";
 $include_path = explode($sep,ini_get("include_path"));
 if (!in_array(".",$include_path)) {
-  setup_exit(sprintf("{t}Please modify your php.ini or add an .htaccess file changing the setting '%s' to '%s' (current value is '%s') !{/t}","include_path",".".$sep.implode($sep,$include_path),ini_get("include_path")),1);
+  setup_exit(sprintf("Please modify your php.ini or add an .htaccess file changing the setting '%s' to '%s' (current value is '%s') !","include_path",".".$sep.implode($sep,$include_path),ini_get("include_path")),1);
 }
 
 $phpversion = "5.2.0";
 if (version_compare(PHP_VERSION, $phpversion, "<")) {
-  setup_exit(sprintf("{t}Setup needs php with at least version %s !{/t} (".PHP_VERSION.")",$phpversion),3);
+  setup_exit(sprintf("Setup needs php with at least version %s ! (".PHP_VERSION.")",$phpversion),3);
 }
 if (version_compare(PHP_VERSION,'5.3','>') and !ini_get('date.timezone')) {
   date_default_timezone_set(@date_default_timezone_get());
@@ -79,8 +80,10 @@ if (!isset($_SERVER["SERVER_ADDR"]) or $_SERVER["SERVER_ADDR"]=="") $_SERVER["SE
 setup::dirs_create_default_folders();
 if (isset($_REQUEST["install"]) and isset($_REQUEST["accept_gpl"]) and $_REQUEST["accept_gpl"]=="yes") {
   install($databases);
-} else {
+} else if (!empty($_REQUEST["lang"])) {
   setup::show_form($databases, !empty($_REQUEST["install"]), !empty($_REQUEST["accept_gpl"]));
+} else {
+  setup::show_lang();
 }
 
 function install($databases) {
@@ -92,46 +95,17 @@ function install($databases) {
   $_SESSION["permission_sql"] = "1=1";
   $_SESSION["permission_sql_read"] = "1=1";
   $_SESSION["permission_sql_write"] = "1=1";
-  
-  @unlink(SIMPLE_STORE."/setup.txt");
-  if ($validate=validate::username($_REQUEST["admin_user"]) and $validate!="") setup::error_add("{t}Admin Username{/t} - {t}validation failed{/t} ".$validate,30);
-  if ($_REQUEST["db_host"]=="") setup::error_add(sprintf("{t}missing field{/t}: %s","{t}Database Hostname / IP{/t}"),31);
-  if ($_REQUEST["db_user"]=="") setup::error_add(sprintf("{t}missing field{/t}: %s","{t}Database User{/t}"),32);
-  if ($_REQUEST["db_name"]=="") setup::error_add(sprintf("{t}missing field{/t}: %s","{t}Database Name{/t}"),33);
-  if ($_REQUEST["admin_pw"]=="") setup::error_add(sprintf("{t}missing field{/t}: %s","{t}Admin Password{/t}"),34);
-  if ($_REQUEST["admin_pw"]!="" and strlen($_REQUEST["admin_pw"])<5) setup::error_add("{t}Admin Password{/t}: {t}Password must be not null, min 5 characters.{/t}","34b");
 
   define("SETUP_DB_TYPE",$_REQUEST["db_type"]);
-
-  if (!@sql_connect($_REQUEST["db_host"], $_REQUEST["db_user"], $_REQUEST["db_pw"], $_REQUEST["db_name"])) {
-    if (!sql_connect($_REQUEST["db_host"], $_REQUEST["db_user"], $_REQUEST["db_pw"])) setup::error_add("{t}Connection to database failed.{/t}\n".sql_error(),35);
-	setup::errors_show();
-	if (!sgsml_parser::create_database($_REQUEST["db_name"])) setup::error_add("{t}Creating database failed.{/t}\n".sql_error(),36);
-  }
-  if (!sql_connect($_REQUEST["db_host"], $_REQUEST["db_user"], $_REQUEST["db_pw"], $_REQUEST["db_name"]) or empty(sys::$db)) {
-    setup::error_add("{t}Connection to database failed.{/t}\n".sql_error(),37);
-	setup::errors_show();
-  }
-
-  if (!$version = sgsml_parser::sql_version()) setup::error_add(sprintf("{t}Could not determine database-version.{/t}"),38);
-  $database_min = (int)substr(str_replace(".","",$databases[SETUP_DB_TYPE][1]),0,3);
-  if ($version < $database_min) setup::error_add(sprintf("{t}Wrong database-version (%s). Please use at least %s !{/t}",$version,$databases[SETUP_DB_TYPE]),"20".SETUP_DB_TYPE);
+  setup::validate_input($databases);  
 
   if (sgsml_parser::table_column_exists("simple_sys_tree","id")) {
     echo '<img src="http://www.simple-groupware.de/cms/logo.php?v='.CORE_VERSION.'&u=1&p='.PHP_VERSION.'_'.PHP_OS.'&d='.SETUP_DB_TYPE.$version.'" style="width:1px; height:1px;">';
   } else {
     echo '<img src="http://www.simple-groupware.de/cms/logo.php?v='.CORE_VERSION.'&u=0&p='.PHP_VERSION.'_'.PHP_OS.'&d='.SETUP_DB_TYPE.$version.'" style="width:1px; height:1px;">';
   }
-  
-  if (SETUP_DB_TYPE=="pgsql") {
-  	if (!sql_query("SELECT ''::tsvector;")) {
-	  setup::error_add("{t}Please install 'tsearch2' for the PostgreSQL database.{/t}\n(Run <postgresql>/share/contrib/tsearch2.sql)\n".sql_error(),21);
-	}
-    if (!sql_query(file_get_contents("modules/core/pgsql.sql"))) setup::error_add("pgsql.sql: ".sql_error(),50);
-  }
-  setup::out(sprintf("{t}Processing %s ...{/t}","schema updates"));
-  setup::errors_show();
 
+  setup::out(t("{t}Processing %s ...{/t}","schema updates"));
   setup_update::change_database_pre();
 
   if (SETUP_DB_TYPE=="sqlite") {
@@ -144,10 +118,10 @@ function install($databases) {
 
   setup_update::change_database_post();
 
-  setup::out(sprintf("{t}Processing %s ...{/t}","sessions"));
+  setup::out(t("{t}Processing %s ...{/t}","sessions"));
   db_delete("simple_sys_session",array(),array());
 
-  setup::out(sprintf("{t}Processing %s ...{/t}","default groups"));
+  setup::out(t("{t}Processing %s ...{/t}","default groups"));
   $groups = array("admin_calendar","admin_news","admin_projects","admin_bookmarks","admin_contacts",
 				  "admin_inventory","admin_helpdesk","admin_organisation","admin_files","admin_payroll",
 				  "admin_surveys","admin_hr","admin_intranet","users_self_registration");
@@ -155,7 +129,7 @@ function install($databases) {
 
   setup_update::database_triggers();
   
-  setup::out(sprintf("{t}Processing %s ...{/t}","folder structure"));
+  setup::out(t("{t}Processing %s ...{/t}","folder structure"));
   $count = db_select_value("simple_sys_tree","id",array());
   if (empty($count)) {
 	$folders = "modules/core/folders.xml";
@@ -173,7 +147,7 @@ function install($databases) {
 
   setup_update::database_folders();
   
-  setup::out(sprintf("{t}Processing %s ...{/t}","config.php"));
+  setup::out(t("{t}Processing %s ...{/t}","config.php"));
   $vars = array(
 	"SETUP_DB_TYPE"=>"'".$_REQUEST["db_type"]."'",
 	"SETUP_DB_HOST"=>"'".$_REQUEST["db_host"]."'",
@@ -193,9 +167,9 @@ function setup_exit($str,$err) {
     <html><body><center>
 	<img src="http://www.simple-groupware.de/cms/logos.php?v='.CORE_VERSION.'&d='.PHP_VERSION.'_'.PHP_OS.'&e='.$err.'" start="width:1px; height:1px;">
     <div style="border-bottom: 1px solid black; letter-spacing: 2px; font-size: 18px; font-weight: bold;">Simple Groupware '.CORE_VERSION_STRING.' - Setup</div>
-	<br><div>{t}Error{/t}:</div>
+	<br><div>'.t("{t}Error{/t}").':</div>
 	<error>'.htmlspecialchars($str, ENT_QUOTES).'</error><br><br>
-	<div><a href="index.php">{t}Relaunch Setup{/t}</a></div>
+	<div><a href="index.php">'.t("{t}Relaunch Setup{/t}").'</a></div>
 	<br><hr>
 	<a href="http://www.simple-groupware.de/cms/Main/Installation" target="_blank">Installation manual</a>
 	<hr><br>
