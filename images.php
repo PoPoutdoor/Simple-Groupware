343bea0e18558a936f5f15787eebb02168fe1ab0
Thomas Bley <t.bley@shobbits.com>
2012-06-10 19:50:14 +0200

    cleanup

--- images.php
+++ images.php
@@ -63,7 +63,11 @@ function image_newcolor($image_file,$file_conf,$newcolor) {
   $rgb = array('r' => hexdec(substr($newcolor,0,2)), 'g' => hexdec(substr($newcolor,2,2)), 'b' => hexdec(substr($newcolor,4,2)));
   $img = greyscale(imagecreatefromgif(sys_custom($image_file)));
   $img = imageselectivesolor($img,floor(($rgb["r"]+30)/2.55),floor(($rgb["g"]+30)/2.55),floor(($rgb["b"]+30)/2.55));
-  download($file_conf,"image/png","",false); 
+  $until = 3600*24*30;
+  header("Content-Type: image/png; charset=utf-8");
+  header("Cache-Control: public, max-age=".$until.", must-revalidate");
+  header("Pragma: public");
+  header("Expires: ".gmdate("D, d M Y H:i:s", NOW+$until)." GMT");
   imagepng($img);
   exit;
 }
@@ -78,7 +82,7 @@ function greyscale($img) {
 }
 
 function imageselectivesolor($img,$red,$green,$blue) {
-  for($i=0;$i<imagecolorstotal($img);$i++) {
+  for($i=0; $i<imagecolorstotal($img); $i++) {
     $col=ImageColorsForIndex($img,$i);
     $red_set=$red/100*$col['red'];
     $green_set=$green/100*$col['green'];
@@ -100,30 +104,5 @@ function predownload($filename,$id) {
   if (!empty($_SERVER["HTTP_IF_NONE_MATCH"]) and $etag == stripslashes($_SERVER["HTTP_IF_NONE_MATCH"])) {
     header("HTTP/1.1 304 Not Modified");
 	exit;
-  }
-}
-
-function download($filename,$mimetype,$output,$show) {
-  $until = 3600*24*30;
-  if (DEBUG) $until = 1;
-  header("Content-Type: ".$mimetype."; charset=utf-8");
-  header("Cache-Control: public, max-age=".$until.", must-revalidate");
-  header("Pragma: public");
-  header("Expires: ".gmdate("D, d M Y H:i:s", NOW+$until)." GMT");
-  if (!$show) return;
-  if ($mimetype=="text/css" and CORE_COMPRESS_OUTPUT and isset($_SERVER["HTTP_ACCEPT_ENCODING"]) and
-	  strpos($_SERVER["HTTP_ACCEPT_ENCODING"],"gzip")!==false and !@ini_get("zlib.output_compression")) {
-	if ($output=="") $hash = "SGS".strlen($filename).crc32($filename).filemtime($filename); else $hash = "SGS".strlen($output).crc32($output);
-	header("Content-Encoding: gzip");
-    $cache_file = SIMPLE_CACHE."/output/".$hash.".cache";
-	if (!file_exists($cache_file) or filesize($cache_file)==0) {
-      if ($output=="") $output = file_get_contents($filename);
-	  $output = gzencode($output);
-	  file_put_contents($cache_file, $output, LOCK_EX);
-	} else $output = file_get_contents($cache_file);
-	exit($output);
-  } else {
-    if ($output=="") $output = file_get_contents($filename);
-	exit($output);
   }
 }
