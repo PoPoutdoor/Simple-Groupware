c723523360f175aee5a03d619456e54f16eb3608
Thomas Bley <thbley+github@gmail.com>
2012-06-22 01:05:18 +0200

    added css conf in php

---
+++ templates/css/css_conf.php
@@ -0,0 +1,193 @@
+<?php
+/**
+ * @package Simple Groupware
+ * @link http://www.simple-groupware.de
+ * @author Thomas Bley
+ * @copyright Copyright (C) 2002-2012 by Thomas Bley
+ * @license GPLv2
+ */
+
+$styles["core"] = array(
+"logo" => "ext/images/sgs_logo.png",
+"bg_login" => "ext/images/bg_autumn.jpg",
+"logo_login" => "ext/images/sgs_logo2.png",
+"logo_link" => "http://www.simple-groupware.de",
+"login_notice" => "Photo from Axel Kristinsson",
+"login_notice_link" => "http://www.flickr.com/photos/axelkr/5047847813/",
+"bg_full" => "",
+
+"bg_grey" => "#F5F5F5",
+"bg_grey_gradient" => "#EEEEEE",
+"bg_dark_grey" => "#E5E5E5",
+"bg_white" => "#FFFFFF",
+"bg_white_fixed" => "#FFFFFF",
+"bg_light_blue" => "#B6BDD2",
+"bg_light_blue2" => "#bbbbdd",
+"bg_red" => "#FFDDDD",
+"bg_red_over" => "#FFCCCC",
+"bg_green" => "#DDFFDD",
+"bg_green_click" => "#88CC88",
+"bg_hl_items" => "#FFC0C0",
+
+"border" => "1px solid #AAAAAA",
+"border_red" => "1px solid #FF0000",
+"border_black" => "1px solid #666666",
+"border_blue" => "1px solid #000080",
+
+"color_menu_black" => "#666666",
+"color_tab_black" => "#666666",
+"color_light_grey" => "#DDDDDD",
+"color_white" => "#FFFFFF",
+"color_grey" => "#808080",
+"color_red" => "#FF0000",
+"color_blue" => "#0000FF",
+
+"button_bg_white" => "#FFFFFF",
+
+"font" => "font-family: Arial, Helvetica, Verdana, sans-serif;",
+"font_black" => "color: #666666;",
+"font_black_bold" => "color: #666666; font-weight: bold;",
+"font_data_black" => "color: #666666;",
+"font_tab_black" => "color: #666666;",
+"font_blue" => "color: #0000FF;",
+"font_grey" => "color: #AAAAAA;",
+"font_light_grey" => "color: #DDDDDD;",
+"font_white" => "color: #FFFFFF;",
+"font_white_bold" => "font-weight: bold; color: #FFFFFF;",
+
+"font_7" => "font-size: 9px;",
+"font_8" => "font-size: 11px;",
+"font_9" => "font-size: 12px;",
+"font_10" => "font-size: 13px;",
+"font_30" => "font-size: 30px;",
+"font_menu" => "font-size: 10px; line-height: 12px;",
+"font_menu_big" => "font-size: 11px; line-height: 13px; letter-spacing: 1px;",
+);
+
+$styles["rtl"] = array(
+"font" => "font-family: Tahoma, Arial, Helvetica, Verdana, sans-serif;",
+"direction" => "direction: RTL;",
+"text_align" => "text-align: right;",
+);
+
+$styles["core_tree_icons"] = array(
+"tree_icons" => "1",
+);
+
+$styles["contrast"] = array(
+"logo" => "ext/images/sgs_logo2.png",
+"color_tab_black" => "#000000",
+"font_black" => "color: #000000;",
+"font_black_bold" => "color: #000000; font-weight: bold;",
+"font_data_black" => "color: #000000;",
+"font_tab_black" => "color: #000000;",
+);
+
+$styles["lake"] = array(
+"font_black" => "color: #AAAAAA;",
+"bg_white" => "#000000",
+"bg_full" => "ext/images/bg_lake.jpg",
+"bg_light_blue" => "#8096BD",
+);
+
+$styles["paradise"] = array(
+"bg_white" => "#000000",
+"bg_full" => "ext/images/bg_paradise.jpg",
+"bg_light_blue" => "#F7D17C",
+"font_black" => "color: #F5F5F5;",
+"font_black_bold" => "color: #E5E5E5; font-weight: bold;",
+);
+
+$styles["earth"] = array(
+"bg_white" => "#000000",
+"bg_full" => "ext/images/bg_earth.jpg",
+"bg_light_blue" => "#7A9CC9",
+"font_black" => "color: #F5F5F5;",
+"font_black_bold" => "color: #E5E5E5; font-weight: bold;",
+);
+
+$styles["water"] = array(
+"bg_white" => "#000000",
+"bg_full" => "ext/images/bg_water.jpg",
+"bg_light_blue" => "#9DA6E7",
+"font_black" => "color: #F5F5F5;",
+"font_black_bold" => "color: #E5E5E5; font-weight: bold;",
+);
+
+$styles["beach"] = array(
+"bg_white" => "#000000",
+"bg_full" => "ext/images/bg_beach.jpg",
+"bg_light_blue" => "#B5D1DD",
+"font_black" => "color: #F5F5F5;",
+"font_black_bold" => "color: #E5E5E5; font-weight: bold;",
+);
+
+$styles["desert"] = array(
+"bg_full" => "ext/images/bg_desert.jpg",
+"bg_light_blue" => "#E9B77A",
+"bg_dark_grey" => "#F3CC8B",
+"bg_grey" => "#FFE0A7",
+"bg_grey_gradient" => "#FFE0A7",
+"color_blue" => "#000000",
+"button_bg_white" => "#FFDB9C",
+"font_black" => "color: #F5F5F5;",
+"font_black_bold" => "color: #F5F5F5; font-weight: bold;",
+"font_blue" => "color: #000000;",
+);
+
+$styles["nature"] = array(
+"bg_white" => "#000000",
+"bg_full" => "ext/images/bg_nature.jpg",
+"bg_light_blue" => "#71C031",
+"color_blue" => "#AF7817",
+"border_red" => "1px solid #00AA00",
+"border_blue" => "1px solid #008000",
+"font_black" => "color: #F5F5F5;",
+"font_black_bold" => "color: #E5E5E5; font-weight: bold;",
+"font_blue" => "color: #AF7817;",
+);
+
+$styles["sunset"] = array(
+"bg_white" => "#000000",
+"bg_full" => "ext/images/bg_sunset.jpg",
+"border" => "1px solid #E5E5E5",
+"border_red" => "1px solid #00FF00",
+"border_black" => "1px solid #666666",
+"border_blue" => "1px solid #FF00FF",
+"bg_grey" => "#FFFFC0",
+"bg_grey_gradient" => "#FFFFC0",
+"bg_dark_grey" => "#C0FFC0",
+"bg_white" => "#000000",
+"bg_light_blue" => "#FFC0C0",
+"button_bg_white" => "#C0FFC0",
+"color_tab_black" => "#808080",
+"color_light_grey" => "#FFFFC0",
+"color_white" => "#FF0000",
+"color_blue" => "#FF40FF",
+"font_black" => "color: #FFFFFF;",
+"font_black_bold" => "color: #F5F5F5; font-weight: bold;",
+"font_data_black" => "color: #0000FF;",
+"font_tab_black" => "color: #C0C0C0;",
+"font_blue" => "color: #FF40FF;",
+"font_grey" => "color: green;",
+"font_light_grey" => "color: #C0C0C0;",
+"font_white" => "color: #FF0000;",
+"font_white_bold" => "font-weight: bold; color: #FF0000;",
+);
+
+$styles["blackwhite"] = array(
+"bg_white" => "#000000",
+"bg_full" => "ext/images/bg_blackwhite.jpg",
+"border" => "1px solid #C0C0C0",
+"border_red" => "1px solid #000000",
+"border_black" => "1px solid #666666",
+"border_blue" => "1px solid #999999",
+"bg_white" => "#000000",
+"bg_light_blue" => "#C0C0C0",
+"color_tab_black" => "#999999",
+"color_blue" => "#666666",
+"font_black" => "color: #F5F5F5;",
+"font_black_bold" => "color: #F5F5F5; font-weight: bold;",
+"font_tab_black" => "color: #999999;",
+"font_blue" => "color: #666666;",
+);
