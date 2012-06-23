480ecf96784e8e51950a0de6c6726c8a689d8b23
Thomas Bley <thbley+github@gmail.com>
2012-06-23 16:54:21 +0200

    replaced smarty with php template

--- templates/cms/sitemap.php
+++ templates/cms/sitemap.php
@@ -1,16 +1,18 @@
-{*
+<?
+/**
  * @package Simple Groupware
  * @link http://www.simple-groupware.de
  * @author Thomas Bley
  * @copyright Copyright (C) 2002-2012 by Thomas Bley
  * @license GPLv2
- *}
+ */
+?>
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-{foreach name=outer item=entry key=key from=$sitemap_pages}
+<? foreach ($this->sitemap_pages as $entry) { ?>
 <url>
-    <loc>{$page.url}{$page.url_param}{$entry.pagename}</loc>
-    <lastmod>{$entry.lastmodified|date_format:"%Y-%m-%d"}</lastmod>
+    <loc><?= $this->page["url"].$this->page["url_param"].$entry["pagename"] ?></loc>
+    <lastmod><?= sys_date("Y-m-d", $entry["lastmodified"]) ?></lastmod>
 </url>
-{/foreach}
+<? } ?>
 </urlset>
