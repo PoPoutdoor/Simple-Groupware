c42d8d1c981dc99ba2b6804981fc7faabb512dbe
Thomas Bley <thbley+github@gmail.com>
2012-11-24 17:41:20 +0100

    renamed build tests

--- tests/Build.php
+++ tests/Build.php
@@ -2,13 +2,13 @@
 
 class Build extends PHPUnit_Framework_TestCase {
 
-static function setUpBeforeClass() {
+public static function setUpBeforeClass() {
 }
   
-function setUp() {
+protected function setUp() {
 }
 
-function testValidateModuleIcons() {
+private function testValidateModuleIcons() {
   $exceptions = array("nodb_calendar_contacts.xml", "nodb_calendar_departments.xml", "nodb_calendar_users.xml",
     "nodb_rights.xml", "nodb_index.xml", "nodb_ldif_contacts.xml", "nodb_pmwiki.xml", "nodb_rights_edit.xml",
     "nodb_schema.xml", "nodb_structure.xml", "search.xml");
